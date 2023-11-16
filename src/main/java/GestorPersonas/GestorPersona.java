77eb5ff4d01538c97da4c680be6f4a3fdf883dca
QuesoRokeFort <133599280+quesorokefort@users.noreply.github.com>
2023-11-15 21:10:10 -0300

    cree el modelo de CLiente y modifique un monton de cosas

--- src/main/java/GestorPersonas/GestorPersona.java
+++ src/main/java/GestorPersonas/GestorPersona.java
@@ -1,18 +1,25 @@
 package GestorPersonas;
 
+import DTO.ClienteDTO;
 import DTO.PersonaDTO;
 import Hibernate.Dao.PersonaDao;
+import Hibernate.Model.Cliente;
 import Hibernate.Model.Persona;
 import Hibernate.Model.Rol;
 
 public class GestorPersona {
 
     public static void crearCliente(PersonaDTO personaDTO) {
+        ClienteDTO cliente = new ClienteDTO();
+        cliente.setAnioRegistro(2023);
+        cliente.setPersona(personaDTO);
+        cliente.setMail(personaDTO.getUsuario().getMail());
+        cliente.setEstadoCliente("activo");
+        cliente.setCondicionIva("buena"); // a mirar nose como funcionan las condiciones de iva
+        cliente.setNroSiniestros(0);
+        personaDTO.setCliente(cliente);
         Persona persona = new Persona(personaDTO);
-        persona.getUsuario().setRol(Rol.CLIENTE);
-        persona.getUsuario().setClave("");
-        persona.getUsuario().setMail(personaDTO.getUsuario().getMail());
-        System.out.println(persona.toString());
+        persona.setUsuario(null);
         PersonaDao.savePersona(persona);
     }
 }
