9c93a6804b2db4bebb91045254c08b74d21492e9
= <yaconangelo2017@gmail.com>
2023-11-17 18:52:48 -0300

    Hice otra vez el menu del productor de seguros, y despues hice el de gerente y cobrador. Luego puse en el gestorinterface el codigo para ejecutar el menu del cobrador y gerente

---
+++ src/main/java/Interface/MenuGerente.java
@@ -0,0 +1,15 @@
+package Interface;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MenuGerente {
+    private JPanel PantallaPrincipal;
+    private JButton generarInformeMensualButton;
+    private JButton generarListadoPropuestasButton;
+    private JButton cerrarSesionButton;
+
+    public Component getPanel3() {
+        return PantallaPrincipal;
+    }
+}
---
+++ src/main/java/Interface/MenuProductorSeguros.java
@@ -0,0 +1,33 @@
+package Interface;
+
+import javax.swing.*;
+
+public class MenuProductorSeguros {
+    private JPanel PantallaPrincipal;
+    private JButton darDeAltaPolizaButton;
+    private JButton darDeAltaClienteButton;
+    private JButton consultarPolizaButton;
+    private JButton consultarClienteButton;
+    private JButton generarPropuestaRenovacionButton;
+    private JButton actualizarFactoresButton;
+    private JButton cerrarSesionButton;
+    private JLabel idMenu;
+    private JPanel idPanelMenu;
+
+    public MenuProductorSeguros() {
+
+
+    };
+
+
+
+    public JPanel getPanel1() {
+        return PantallaPrincipal;
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
+
+
