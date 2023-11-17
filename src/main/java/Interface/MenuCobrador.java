453e61c3041ddb9efa26d3f0de7bb550ede658ab
= <yaconangelo2017@gmail.com>
2023-11-17 18:52:48 -0300

    Hice otra vez el menu del productor de seguros, y despues hice el de gerente y cobrador. Luego puse en el gestorinterface el codigo para ejecutar el menu del cobrador y gerente

---
+++ src/main/java/Interface/MenuCobrador.java
@@ -0,0 +1,15 @@
+package Interface;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MenuCobrador {
+    private JPanel PantallaPrincipal;
+    private JButton registrosDePagoPolizaButton;
+    private JButton cerrarSesionButton;
+    private JPanel PanelMenu;
+
+    public Component getPanel2() {
+        return PantallaPrincipal;
+    }
+}
