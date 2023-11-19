e539205f2a76d39a245fcf64db78e7df64873252
= <yaconangelo2017@gmail.com>
2023-11-19 18:25:38 -0300

    Interaces hechas

---
+++ src/main/java/Interface/AltaPoliza1.java
@@ -0,0 +1,11 @@
+package Interface;
+
+import javax.swing.*;
+
+public class AltaPoliza1 {
+    private JButton buscarClienteButton;
+    private JButton agregarClienteButton;
+    private JPanel PantallaPrincipal;
+    private JLabel a999999Label;
+    private JButton confirmarButton;
+}
---
+++ src/main/java/Interface/AltaPoliza2.java
@@ -0,0 +1,13 @@
+package Interface;
+
+import javax.swing.*;
+
+public class AltaPoliza2 {
+    private JPanel PanelPrincipal;
+    private JComboBox comboBox1;
+    private JButton button1;
+    private JTextField textField2;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JButton xButton;
+}
